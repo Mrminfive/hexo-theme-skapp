9297f633694a6e4e7b3bd8ac61f50c4b3967b957
minfive <chenxiaowu1994@outlook.com>
2017-08-27 03:40:58 +0800

    init sidebar

--- scripts/helper.js
+++ scripts/helper.js
@@ -141,6 +141,51 @@ hexo.extend.helper.register('num_toArray', function(num) {
     return ret;
 });
 
-hexo.extend.helper.register('get_setting', function(key) {
-    return this.config[key] || this.theme[key];
+hexo.extend.helper.register('get_setting', function(...keys) {
+    return keys.reduce((p, key) => (p || this.config[key] || this.theme[key]), void 0);
+});
+
+hexo.extend.helper.register('tags', function() {
+    let tags = this.site.tags.sort('name', 1).filter(tag => tag.length);
+
+    return `
+        <ul class="block-list tag-list clearfix">
+            ${ tags.reduce((p, item) => {
+                return p + `
+                    <li class="tag-item">
+                        <a class="tag-link" href="${ this.url_for(item.path) }">${ item.name }</a>
+                    </li>  
+                `;
+            }, '') }
+        </ul>
+    `;
+});
+
+hexo.extend.helper.register('latest_post', function(len = 4) {
+    const getTime = date => (new Date(this.date_xml(date))).getTime();
+
+    let posts = this.site.posts.data
+        .filter(post => !post.draft)
+        .sort((first, next) => getTime(next.date) - getTime(first.date))
+        .slice(0, len);
+
+    return `
+        <ul class="block-list latest-post-list">
+            ${ posts.reduce((p, post) => {
+                return p + `
+                    <li class="latest-post-item">
+                        <a href="${ this.url_for(post.path) }" title="${ post.title }">
+                            <div class="item__cover">
+                                <img src="${ post.cover }" alt="${ post.title }" />
+                            </div>
+                            <div class="item__info">
+                                <h3 class="item__title">${ post.title }</h3>
+                                <span class="item__text">${ this.date(post.date) }</span>
+                            </div>
+                        </a>
+                    </li>
+                `;
+            }, '') }
+        </ul>
+    `;
 });
