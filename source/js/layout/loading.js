a8253db6b23121e46ffe37d3274c30e3dea3fa44
IT小生 <niudear@foxmail.com>
2018-01-03 23:40:20 +0800

    Bugfix/loadding (#31)
    * 修复loadding阻塞页面bug

--- source/js/layout/loading.js
+++ source/js/layout/loading.js
@@ -1,11 +1,24 @@
 // loading
-window.addEventListener('load', function() {
-    (function() {
-        var
-            page = document.getElementById('page'),
-            loading = document.getElementById('page-loading');
+document.onreadystatechange = function(){
+    if (document.readyState == 'interactive') {
+        window.setTimeout(function(){
+            disableLoad()
+        },4000)
+    }
 
-        loading.classList.add('js-hidden');
-        page.classList.remove('js-hidden');
-    })()
-});
+    if (document.readyState == 'complete') {
+        var page = document.getElementById('page')
+        if (page.classList.contains('js-hidden')) {
+            disableLoad()
+        }
+    }
+}
+
+function disableLoad(){
+    var
+    page = document.getElementById('page'),
+    loading = document.getElementById('page-loading');
+
+    loading.classList.add('js-hidden');
+    page.classList.remove('js-hidden');
+}
